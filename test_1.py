baec7d2eaf32bf8970b3399a144b884b6fc0ad06
llliwwdddsa <3451892768@qq.com>
2025-09-28 20:01:33 +0800

    save_9_28

--- test_1.py
+++ test_1.py
@@ -5,3 +5,22 @@ total = item_one + \
         item_two + \
         item_three
 print(total)
+print(r"123\n123")
+
+str = '12345678'
+print(str[0:-1])
+print(str[2:5])
+print(str[2:5:2])
+print(str * 2)
+print('----------------------')
+
+
+# The default output of print is a new line
+
+print(123)
+print(456)
+
+print(123, end=" ")
+print(456, end=" ")
+
+
